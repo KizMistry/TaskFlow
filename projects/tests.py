da6fc7c6f4ca79d2f6e92e7114b1834ba37a74ed
KizMistry <Kiz_mistry@hotmail.com>
2023-06-23 19:20:43 +0000

    Add Project Detail View Tests in tests.py

--- projects/tests.py
+++ projects/tests.py
@@ -26,3 +26,36 @@ class ProjectListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_posts(self):
         response = self.client.post('/projects/', {'title': 'Project Test'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class ProjectDetailViewTests(APITestCase):
+    def setUp(self):
+        kiran = User.objects.create_user(username='kiran', password='pass')
+        jordan = User.objects.create_user(username='jordan', password='pass')
+        Project.objects.create(
+            owner=kiran, title='Project Test', description='Kirans project details'
+        )
+        Project.objects.create(
+            owner=jordan, title='Project Test', description='Jordans project details'
+        )
+
+    def test_can_retrieve_project_using_valid_id(self):
+        response = self.client.get('/projects/1/')
+        self.assertEqual(response.data['title'], 'Project Test')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_project_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_project(self):
+        self.client.login(username='kiran', password='pass')
+        response = self.client.put('/projects/1/', {'title': 'TaskFlow'})
+        project = Project.objects.filter(pk=1).first()
+        self.assertEqual(project.title, 'TaskFlow')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_project(self):
+        self.client.login(username='kiran', password='pass')
+        response = self.client.put('/projects/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
