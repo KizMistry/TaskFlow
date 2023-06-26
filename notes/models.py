c94a66f8988575d6469513112997690b8e463132
KizMistry <Kiz_mistry@hotmail.com>
2023-06-26 18:13:00 +0000

    add Note model to notes app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tasks.models import Task
+from projects.models import Project
 
-# Create your models here.
+
+class Note(models.Model):
+    """
+    Notes model, related to User, Project and Task
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
