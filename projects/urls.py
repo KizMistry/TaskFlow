56dc9adba5b3a9251a57ae31590eebcb7805a4cc
KizMistry <Kiz_mistry@hotmail.com>
2023-06-26 19:37:04 +0000

    refactor views.py for profiles and projects app

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from projects import views
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.PorjectDetail.as_view()),
+    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
 ]
