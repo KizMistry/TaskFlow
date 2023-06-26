4cbe5c17fe4bcf4fc56e312558f0a6863636e2d6
KizMistry <Kiz_mistry@hotmail.com>
2023-06-26 18:33:05 +0000

    add NoteList and NoteDetail to notes/views.py

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from taskflow.permissions import IsOwnerOrReadOnly
+from .models import Note
+from .serializers import NoteSerializer, NoteDetailSerializer
 
-# Create your views here.
+
+class NoteList(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Note.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Note.objects.all()
