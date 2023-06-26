af3d00658a8a1d1959d97b645533d24ebc399eb5
KizMistry <Kiz_mistry@hotmail.com>
2023-06-26 18:25:24 +0000

    add Note serializers to notes app

---
+++ notes/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from notes.models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethod()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    
+class Meta:
+    model = Note
+    fields =[
+        'id', 'owner', 'is_owner', 'project', 'content',
+        'created_at', 'updated_at', 'task', 'profile_id',
+        'profile_image',
+    ]
+
+
+class NoteDetailSerializer(NoteSerializer):
+    project = serializers.ReadOnlyField(source='project.id')
+    task = serializers.ReadOnlyField(source='task.id')
