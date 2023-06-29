2c4b394a05aaa8e14f3a6cef1de7760f1d9d97b3
KizMistry <Kiz_mistry@hotmail.com>
2023-06-29 19:50:20 +0000

    add projects, follower, following count to profile
    add counts to profile views.py & serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
 from .models import Profile
+from collaborators.models import Collaborator
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    projects_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Collaborator.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'bio', 'image', 'profession', 'is_owner',
+            'bio', 'image', 'profession', 'is_owner', 'projects_count',
+            'followers_count', 'following_count', 'following_id',
         ]
