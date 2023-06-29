cb70b4b08d4a9057bf2f2f9533fddc9f032552fe
KizMistry <Kiz_mistry@hotmail.com>
2023-06-29 17:29:26 +0000

    change allowed host to variable
    add CLIENT_ORIGIN_DEV to access API when developing client-side app

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -65,7 +66,8 @@ ALLOWED_HOSTS = [
     '8000-kizmistry-taskflow-40gpd5dna5d.ws-eu101.gitpod.io',
     'localhost',
     'taskflow-app.herokuapp.com',
-    'https://taskflow-app-fe0b343d965d.herokuapp.com/',]
+    os.environ.get('ALLOWED_HOST'),
+    ]
 
 
 # Application definition
@@ -110,9 +112,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
