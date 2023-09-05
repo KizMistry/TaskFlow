3a98287ff4f7cab0c74ec2f456ed4442f76012b6
Kiran Mistry <108070195+KizMistry@users.noreply.github.com>
2023-09-05 18:31:27 +0100

    Update tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ProfileListViewTests(APITestCase):
+  def setUp(self):
+    User.objects.create_user(username='billy', password='pass')
+
+  def test_can_list_tasks(self):
+    kiran = User.objects.get(username='kiran')
+    Task.objects.create(owner=billy, title='Task Test')
+    response = self.client.get('/tasks/')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    print(response.data)
+    print(len(response.data))
